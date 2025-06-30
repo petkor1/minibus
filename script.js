7f316a846161387fa027e4b52b3c51699775a864
Piotr Korbas <piotrkorbas@gmail.com>
2025-06-30 09:10:19 +0200

    małą zmiana nazwy filtra

--- script.js
+++ script.js
@@ -181,7 +181,7 @@ document.addEventListener('DOMContentLoaded', () => {
     return `
         <div class="filter-section">
             <button type="button" class="filter-toggle-btn">
-                <span>Filtruj odjazdy</span>
+                <span>Filtruj</span>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" /></svg>
             </button>
             <div class="filter-container">

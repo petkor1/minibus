d2bbd9e511721d3dc4762b2f555131219a2c3351
Piotr Korbas <piotrkorbas@gmail.com>
2025-07-01 16:28:26 +0200

    wersja 3.0.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.{html,js}", // Skanuj wszystkie pliki .html i .js w głównym folderze
+    "./*.{html,js}", // To pozostaje bez zmian
+  ],
+  // NOWA SEKCJA: Dodaj tę listę bezpiecznych klas
+  safelist: [
+    'bg-orange-100',
+    'text-orange-700',
+    'border-orange-500',
+    'z-10',
+    'focus:z-20',
+    // Klasy dla listy rozwijanej (na wszelki wypadek)
+    'focus:border-orange-500',
+    'focus:ring-1',
+    'focus:ring-orange-500',
   ],
   theme: {
     extend: {},
